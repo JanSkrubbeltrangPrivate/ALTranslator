ee4ddfb9737a9fe89806c3be97514f4f6ddf4dd5
Jan Skrubbeltrang <jan.skrubbeltranmg@gmail.com>
2023-05-03 19:41:05 +0200

    AppView Implementet

--- WPF/GUI/Views/AppView.xaml.cs
+++ WPF/GUI/Views/AppView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace GUI.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AppView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AppView : Window
     {
-        public MainWindow()
+        public AppView()
         {
             InitializeComponent();
         }
---
+++ WPF/VML/Models/BaseNotifyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace VML.Models
+{
+    public abstract class BaseNotifyModel : INotifyPropertyChanged
+    {
+        #region INotifyMember
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        #endregion
+    }
+}
